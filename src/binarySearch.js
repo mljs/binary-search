fd27ff587de98f9b61c2dde7b6c76d153dd9e964
Michaël Zasso <targos@protonmail.com>
2016-09-01 14:24:29 +0200

    feat: add from and to options

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -1,12 +1,13 @@
 /**
  * Performs a binary search of value in array
- * @param array - Array in which value will be searched. It must be sorted.
- * @param value - Value to search in array
+ * @param {number[]} array - Array in which value will be searched. It must be sorted.
+ * @param {number} value - Value to search in array
  * @return {number} If value is found, returns its index in array. Otherwise, returns a negative number indicating where the value should be inserted: -(index + 1)
  */
-function binarySearch(array, value) {
-    var low = 0;
-    var high = array.length - 1;
+function binarySearch(array, value, options) {
+    options = options || {};
+    var low = options.from || 0;
+    var high = options.to || array.length - 1;
 
     while (low <= high) {
         var mid = (low + high) >>> 1;
